254995c89cd7765c752900b264e0aacd1a219ed4
Javier <Javier@DESKTOP-82RJPB2.home>
2022-02-23 20:34:14 +0100

    new

---
+++ src/geo/src/FiguraGeometrica.java
@@ -0,0 +1,25 @@
+package geo.src;
+
+
+public abstract class FiguraGeometrica {
+
+	protected String tipoFigura;
+
+	public FiguraGeometrica(String tipoFigura) {
+		super();
+		this.tipoFigura = tipoFigura;
+	}
+
+	public abstract double area();
+	
+	public abstract double perimetro();
+
+	public String getTipoFigura() {
+		return tipoFigura;
+	}
+
+	public void setTipoFigura(String tipoFigura) {
+		this.tipoFigura = tipoFigura;
+	}
+
+}
