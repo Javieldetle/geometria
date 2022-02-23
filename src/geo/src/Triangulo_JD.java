61ce019b0dd46f7abe50314e10257b16912f7a33
Javier <Javier@DESKTOP-82RJPB2.home>
2022-02-23 20:48:16 +0100

    "método en una línea" (inline).

--- src/geo/src/Triangulo_JD.java
+++ src/geo/src/Triangulo_JD.java
@@ -21,11 +21,7 @@ public class Triangulo_JD extends FiguraGeometrica_JD {
 	@Override
 	public double area() { //Utiliza la formula de Her—ns
 		double sp;
-		sp = this.semiPerimetro();
+		sp = this.perimetro() / 2;
 		return Math.sqrt(sp * (sp-lado1) * (sp-lado2) * (sp-lado3));
-	}
-	
-	public double semiPerimetro() {
-		return this.perimetro() / 2;
 	}
 }
